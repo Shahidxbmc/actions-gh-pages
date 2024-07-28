6f82517461534839b7120b3b87acfd45472c6a65
Shahidxbmc <shahid.xbmc@gmail.com>
2024-07-28 19:06:27 -0400

    Update a.py

--- a.py
+++ a.py
@@ -1,10 +1,7 @@
-from pytubefix import YouTube
-
-url="https://m.youtube.com/watch?v=bNyUyrR0PHo"
-yt = YouTube(url)
-#stream = print(yt.streams.get_highest_resolution()
-print(yt.streams.all()[0].url)
 
 
-
-       
+<h1><a class='chlist' href='http://127.0.0.1:7776/gettubech=/channel/UCNye-wNBqNL5ZzHSJj3l8Bg'> Aljazeera English</a></h1>
+<h1><a class='chlist' href='http://127.0.0.1:7776/gettubech=/channel/UCfiwzLy-8yKzIbsmZTzxDgw'> Aljazeera arabic</a></h1>
+<h1><a class='chlist' href='http://127.0.0.1:7776/gettubech=/channel/UCknLrEdhRCp1aegoMqRaCZg'> DW English</a></h1>
+<h1><a class='chlist' href='http://127.0.0.1:7776/gettubech=/channel/UC30ditU5JI16o5NbFsHde_Q'> DW Arabic</a></h1>
+<h1><a class='chlist' href='http://127.0.0.1:7776/gettubech=/channel/UCDPk9MG2RexnOMGTD-YnSnA'> Nationa Geographic Wild</a></h1>
